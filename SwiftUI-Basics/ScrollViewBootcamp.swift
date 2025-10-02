110d0da6c3506fc91f72579c7f1c7d66b0606051
Dhathri33 <dbathini@luc.edu>
2025-10-02 16:23:14 -0500

    ScrollView Bootcamp

---
+++ SwiftUI-Basics/ScrollViewBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUI-Basics
+//
+//  Created by Dhathri Bathini on 10/2/25.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<10) { index in
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        HStack {
+                            ForEach(0..<20) { index in
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(Color.white)
+                                    .frame(width: 200, height: 150)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewBootcamp()
+}
