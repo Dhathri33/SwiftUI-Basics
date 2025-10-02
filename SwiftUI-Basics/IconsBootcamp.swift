e892b14ae5569f37b7a2585d1f4ef63a01ce7f09
Dhathri33 <dbathini@luc.edu>
2025-10-02 11:20:49 -0500

    Icons bootcamp

---
+++ SwiftUI-Basics/IconsBootcamp.swift
@@ -0,0 +1,25 @@
+//
+//  IconsBootcamp.swift
+//  SwiftUI-Basics
+//
+//  Created by Dhathri Bathini on 10/2/25.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Image(systemName: "paperplane.fill")
+            .resizable()
+           // .font(.largeTitle)
+            .font(.system(size: 200))
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.red)
+            .frame(width: 300, height: 300)
+            //.clipped()
+    }
+}
+
+#Preview {
+    IconsBootcamp()
+}
