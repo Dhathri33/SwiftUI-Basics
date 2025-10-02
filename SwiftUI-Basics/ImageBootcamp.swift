eec23b21fce8d6439133284c29467a1b4a8b2ff9
Dhathri33 <dbathini@luc.edu>
2025-10-02 11:31:48 -0500

    Image Bootcamp

---
+++ SwiftUI-Basics/ImageBootcamp.swift
@@ -0,0 +1,32 @@
+//
+//  ImageBootcamp.swift
+//  SwiftUI-Basics
+//
+//  Created by Dhathri Bathini on 10/2/25.
+//
+
+import SwiftUI
+
+struct ImageBootcamp: View {
+    var body: some View {
+        Image("tiger")
+            
+            .resizable()
+            //.aspectRatio(contentMode: .fit)
+            //.scaledToFit()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+            //.clipped()
+            //.cornerRadius(100)
+            .clipShape(
+                // Circle()
+              // RoundedRectangle(cornerRadius: 15)
+               // Ellipse()
+                Circle()
+            )
+    }
+}
+
+#Preview {
+    ImageBootcamp()
+}
