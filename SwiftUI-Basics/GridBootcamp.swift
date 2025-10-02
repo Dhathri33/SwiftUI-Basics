b7a05455dc411217edef6b6e9f5ac25d93a3a9eb
Dhathri33 <dbathini@luc.edu>
2025-10-02 16:50:48 -0500

    GridBootcamp

---
+++ SwiftUI-Basics/GridBootcamp.swift
@@ -0,0 +1,64 @@
+//
+//  GridBootcamp.swift
+//  SwiftUI-Basics
+//
+//  Created by Dhathri Bathini on 10/2/25.
+//
+
+import SwiftUI
+
+struct GridBootcamp: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 10, alignment: nil),
+        GridItem(.flexible(), spacing: 10, alignment: nil),
+        GridItem(.flexible(), spacing: 10, alignment: nil)
+    ]
+    var body: some View {
+        ScrollView {
+            Rectangle()
+                .fill(Color.orange)
+                .frame(height: 400)
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 6,
+                      pinnedViews: [.sectionHeaders],
+                      content: {
+                Section(header:
+                            Text("Section 1")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.blue)
+                    .padding()
+            ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .frame( height: 150)
+                    }
+                }
+                Section(header:
+                            Text("Section 2")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.red)
+                    .padding()
+            ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .fill(Color.yellow)
+                            .frame( height: 150)
+                    }
+                }
+            }
+            )
+            
+        }
+    }
+}
+
+#Preview {
+    GridBootcamp()
+}
